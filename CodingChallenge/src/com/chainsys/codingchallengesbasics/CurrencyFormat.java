9549b8684ef3bcae597b85e109ffe9d0ca44e1d2
Raks3556 <Raks3556@CSL0699.global.chainsys.com>
2024-05-20 17:55:33 +0530

    Coding Challenge in HakerRank introduction completed

---
+++ CodingChallenge/src/com/chainsys/codingchallengesbasics/CurrencyFormat.java
@@ -0,0 +1,30 @@
+package com.chainsys.codingchallengesbasics;
+	import java.util.*;
+	import java.text.*;
+
+	public class CurrencyFormat {
+	    
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        double payment = scanner.nextDouble();
+	        scanner.close();
+	        
+	        // Write your code here.
+	        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+	        String us = n.format(payment);
+	        NumberFormat n1 = NumberFormat.getCurrencyInstance(Locale.CHINA);
+	        String china = n1.format(payment);
+	        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+	        String france = n2.format(payment);
+	        
+	        NumberFormat n3 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+	        String india = n3.format(payment); 
+	        
+	        System.out.println("US: " + us);
+	        System.out.println("India: " + india);
+	        System.out.println("China: " + china);
+	        System.out.println("France: " + france);
+	    }
+	}
+
+
